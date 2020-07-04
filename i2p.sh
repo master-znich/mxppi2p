a57ce68db22b49a7bb0b2c272f32dfab63da71f0
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-04 22:35:45 +0300

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -11,12 +11,12 @@ sudo apt-get install i2pd -y
 sudo bash -c "cat /tmp/mxppi2p/i2p_conig.conf >> /var/lib/i2pd/tunnels.conf "
 service i2pd restart
 #
-wget -P /usr/lib/prosody/modules https://raw.githubusercontent.com/majestrate/mod_darknet/master/mod_darknet.lua
+sudo "wget -P /usr/lib/prosody/modules https://raw.githubusercontent.com/majestrate/mod_darknet/master/mod_darknet.lua'
 #адресса и2р сервера
-sudo grep "New private keys file" /var/log/i2pd/i2pd.log | sudo grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 | sudo cat >> /tmp/mxppi2p/server.b32.i2p.txt
+sudo "grep "New private keys file" /var/log/i2pd/i2pd.log | sudo grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 | sudo cat >> /tmp/mxppi2p/server.b32.i2p.txt"
 sudo sh /tmp/mxppi2p/rename.sh
 #Теперь редактируем конфиг /etc/prosody/prosody.cfg.lua.
-sudo cat /tmp/mxppi2p/prosody.cfg.lua >> /etc/prosody/prosody.cfg.lua
+sudo "cat /tmp/mxppi2p/prosody.cfg.lua >> /etc/prosody/prosody.cfg.lua"
 #    Генерируем сертификаты шифрования
-sudo bash /tmp/mxppi2p/certs.b32.i2p.sh
+sudo "bash /tmp/mxppi2p/certs.b32.i2p.sh"
 service prosody restart
