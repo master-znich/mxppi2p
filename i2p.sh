6d7ec4fb8d6fe6f6726b413bf23f872da4ed97d7
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-05 14:30:49 +0300

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -26,8 +26,6 @@ sudo "cat /tmp/mxppi2p/prosody.cfg.lua > /etc/prosody/prosody.cfg.lua"
 #замінна xxx.b32.i2p на домен нашого сервера
 read /tmp/mxppi2p/server.b32.i2p.txt << data-file
 sudo echo "server.b32.i2p.txt = $ser"
-sudo sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua
-sudo sed s/xxx.b32.i2p/$ser/g  cat >> /home/test/certs.b32.i2p.sh
-sudo bash /home/test/certs.b32.i2p.sh
-sudo bash /tmp/mxppi2p/certs.b32.i2p.sh
+sudo sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua | sudo sed s/xxx.b32.i2p/$ser/g  cat >> /home/test/certs.b32.i2p.sh
+sudo bash /home/test/certs.b32.i2p.sh | sudo bash /tmp/mxppi2p/certs.b32.i2p.sh
 service prosody restart
