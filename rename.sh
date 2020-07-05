6da26df6803d5ee838f1eb873aaeabbad6cb4248
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-06 01:19:28 +0300

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 #замінна xxx.b32.i2p на домен нашого сервера
 read /home/test/server.b32.i2p.txt <data-file
-sudo echo "server.b32.i2p = $ser"
+sudo echo "server.b32.i2p.txt = $ser"
 sudo sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua
 #sleep
 sudo sed s/xxx.b32.i2p/$ser/g  cat >> /home/test/certs.b32.i2p.sh
