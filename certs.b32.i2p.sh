3463da9269a841c2ae218626abbc0eb059a3d407
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-06 19:39:12 +0300

    Update certs.b32.i2p.sh

--- certs.b32.i2p.sh
+++ certs.b32.i2p.sh
@@ -3,4 +3,5 @@ sudo openssl genrsa -out /etc/prosody/certs/xxx.b32.i2p.key 2048
 sudo openssl req -new -x509 -key /etc/prosody/certs/xxx.b32.i2p.key -out /etc/prosody/certs/xxx.b32.i2p.crt -days 3650
 sudo chown root:prosody /etc/prosody/certs/*.b32.i2p.{key,crt}
 sudo chmod 640 /etc/prosody/certs/*.b32.i2p.{key,crt}
+service prosody restart
 sudo prosodyctl adduser admin@xxx.b32.i2p
