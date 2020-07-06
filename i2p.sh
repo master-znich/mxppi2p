f50138d16717decfe2399e1f6a1943a82a0e390c
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-06 16:37:24 +0300

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -1,15 +1,16 @@
 #!bin/bash
-sudo apt-get install apt-transport-https lua-bit32 prosody  -y
+apt-get install apt-transport-https lua-bit32 prosody  -y
 wget -q -O - https://repo.i2pd.xyz/.help/add_repo
-sudo apt-get update
+apt-get update
 echo install i2pd
-sudo bash -c "cat /tmp/mxppi2p/i2p.conf > /var/lib/i2pd/tunnels.conf "
-sudo service i2pd restart
-wget -P /usr/lib/prosody/modules https://raw.githubusercontent.com/majestrate/mod_darknet/master/mod_darknet.lua
-sudo grep "New private keys file" /var/log/i2pd/i2pd.log | sudo grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 | sudo tee -a  > /tmp/mxppi2p/server.b32.i2p.txt
+apt install i2pd
+bash -c "cat /tmp/mxppi2p/i2p.conf > /var/lib/i2pd/tunnels.conf "
+service i2pd restart
+wget -P /usr/lib/prosody/modules https://raw.githubusercontent.com/majestrate/modhttps://tgraph.io/CHerno-treugolnie-04-14_darknet/master/mod_darknet.lua
+grep "New private keys file" /var/log/i2pd/i2pd.log |  grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 |  tee -a  > /tmp/mxppi2p/server.b32.i2p.txt
 tee -a /tmp/mxppi2p/prosody.cfg.lua > /etc/prosody/prosody.cfg.lua
 read /tmp/mxppi2p/server.b32.i2p.txt << data-file &&
-&& sudo echo "server.b32.i2p.txt = $ser" &&
-sudo sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua | sudo sed s/xxx.b32.i2p/$ser/g  cat >> /home/test/certs.b32.i2p.sh
-sudo bash /home/test/certs.b32.i2p.sh | sudo bash /tmp/mxppi2p/certs.b32.i2p.sh
+&&  echo "server.b32.i2p.txt = $ser" &&
+sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua |  sed s/xxx.b32.i2p/$ser/g  cat >> /home/test/certs.b32.i2p.sh
+bash /home/test/certs.b32.i2p.sh |  bash /tmp/mxppi2p/certs.b32.i2p.sh
 service prosody restart
