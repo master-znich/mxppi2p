4ee924c89556b112173f50a54f9d0c8b497132c6
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-06 19:31:48 +0300

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -10,7 +10,7 @@ bash -c "cat /tmp/mxppi2p/i2p.conf > /var/lib/i2pd/tunnels.conf "
 service i2pd restart
 wget -P /usr/lib/prosody/modules wget https://raw.githubusercontent.com/majestrate/mod_darknet/master/mod_darknet.lua
 grep "New private keys file" /var/log/i2pd/i2pd.log |  grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 |  tee -a  > /tmp/mxppi2p/server.b32.i2p.txt
-tee -a /tmp/mxppi2p/prosody.cfg.lua > /etc/prosody/prosody.cfg.lua
+bash -c "cat /tmp/mxppi2p/prosody.cfg.lua > /etc/prosody/prosody.cfg.lua"
 while read /tmp/mxppi2p/server.b32.i2p.txt << data-file | echo "server.b32.i2p.txt = $ser"
 sed s/xxx.b32.i2p/$ser/g  cat >> /etc/prosody/prosody.cfg.lua 
 sed s/xxx.b32.i2p/$ser/g  cat >> /tmp/mxppi2p/certs.b32.i2p.sh
