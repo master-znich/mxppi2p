a1cccc93ba7e7f310c9b71e6ebbde848fd02b4e9
master-znich <67830362+master-znich@users.noreply.github.com>
2020-07-06 17:19:17 +0300

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install apt-transport-https lua-bit32 prosody i2p -y
 bash -c "cat /tmp/mxppi2p/i2p.conf > /var/lib/i2pd/tunnels.conf "
 service i2pd restart
-wget -P /usr/lib/prosody/modules https://raw.githubusercontent.com/majehttps://www.startpage.com/do/dsearch?query=%D0%BD%D0%B5%D1%82%D1%83+%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%B0+%D1%82%D1%83%D0%BD%D0%B5%D0%BB%D1%8F+i2p&cat=web&pl=opensearch&language=englishstrate/mod_darknet/master/mod_darknet.lua
+wget -P /usr/lib/prosody/modules wget https://raw.githubusercontent.com/majestrate/mod_darknet/master/mod_darknet.lua
 grep "New private keys file" /var/log/i2pd/i2pd.log |  grep -Eo "([a-z0-9]+).b32.i2p" | tail -n1 |  tee -a  > /tmp/mxppi2p/server.b32.i2p.txt
 tee -a /tmp/mxppi2p/prosody.cfg.lua > /etc/prosody/prosody.cfg.lua
 read /tmp/mxppi2p/server.b32.i2p.txt << data-file &&
